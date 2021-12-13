a762f2f2afbdfe928de0064cfd560d567e2f66ef
Ömer Hodo <omerhodo@gmail.com>
2021-12-13 17:41:53 +0300

    ejs and express module added

--- app.js
+++ app.js
@@ -4,8 +4,19 @@ const app = express();
 
 const port = 3000;
 
+// template engine
+app.set("view engine", "ejs");
+
+// middlewares
+app.use(express.static("public"));
+
+// routes
 app.get("/", (req, res) => {
-  res.status(200).send("Hello there!");
+  res.status(200).render("index", { page_name: "index" });
+});
+
+app.get("/about", (req, res) => {
+  res.status(200).render("about", { page_name: "about" });
 });
 
 app.listen(port, () => {
