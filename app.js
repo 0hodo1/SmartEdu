ede9598abc60ea3fa1c15e418e5dd730f673c8fb
Ömer Hodo <omerhodo@gmail.com>
2021-12-19 17:54:42 +0300

    flashMessages added

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const session = require("express-session");
 const MongoStore = require("connect-mongo");
+const flash = require("connect-flash");
 
 // my module
 const pageRoute = require("./routes/pageRoute");
@@ -40,6 +41,11 @@ app.use(
     }),
   })
 );
+app.use(flash());
+app.use((req, res, next) => {
+  res.locals.flashMessages = req.flash();
+  next();
+});
 
 // routes
 app.use("*", (req, res, next) => {
--- controllers/pageController.js
+++ controllers/pageController.js
@@ -22,7 +22,8 @@ exports.getContactPage = (req, res) => {
 };
 
 exports.sendEmail = async (req, res) => {
-  const outputMessage = `
+  try {
+    const outputMessage = `
   <h1>Mail Details</h1>
   <ul>
   <li>Name: ${req.body.name}</li>
@@ -32,31 +33,37 @@ exports.sendEmail = async (req, res) => {
   <p>${req.body.message}</p>
   `;
 
-  // create reusable transporter object using the default SMTP transport
-  let transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 465,
-    secure: true, // true for 465, false for other ports
-    auth: {
-      user: "hodoemail@gmail.com", // gmail account
-      pass: "gvqeygegatwvujso", // gmail password
-    },
-  });
+    // create reusable transporter object using the default SMTP transport
+    let transporter = nodemailer.createTransport({
+      host: "smtp.gmail.com",
+      port: 465,
+      secure: true, // true for 465, false for other ports
+      auth: {
+        user: "hodoemail@gmail.com", // gmail account
+        pass: "gvqeygegatwvujso", // gmail password
+      },
+    });
 
-  // send mail with defined transport object
-  let info = await transporter.sendMail({
-    from: '"Smart Edu Contact from" <hodoemail@gmail.com>', // sender address
-    to: "workhodo@gmail.com", // list of receivers
-    subject: "Smart Edu New Message", // Subject line
-    html: outputMessage, // html body
-  });
+    // send mail with defined transport object
+    let info = await transporter.sendMail({
+      from: '"Smart Edu Contact from" <hodoemail@gmail.com>', // sender address
+      to: "workhodo@gmail.com", // list of receivers
+      subject: "Smart Edu New Message", // Subject line
+      html: outputMessage, // html body
+    });
 
-  console.log("Message sent: %s", info.messageId);
-  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+    console.log("Message sent: %s", info.messageId);
+    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
 
-  // Preview only available when sending through an Ethereal account
-  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-  // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+    // Preview only available when sending through an Ethereal account
+    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+    // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
 
-  res.status(200).redirect("/contact");
+    req.flash("success", "We received your message succesfully");
+    res.status(200).redirect("/contact");
+  } catch (err) {
+    // req.flash("error", `Somethib happend! ${err}`);
+    req.flash("error", `Somethib happend!`);
+    res.status(400).redirect("/contact");
+  }
 };
